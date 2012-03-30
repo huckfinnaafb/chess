346d2651062b86fbd0eff2d11c6a54c2bf48be32
Samuel Ferrell <huckfinnaafb@gmail.com>
2012-03-30 07:32:24 +0000

    getters and setters

--- src/piece/piece.js
+++ src/piece/piece.js
@@ -3,11 +3,43 @@ define(function (require) {
     var movement = require("piece/movement");
 
     function Piece(type, color, position) {
-        this.type = type;
         this.color = color;
+        this.movement = movement[type];
         this.position = position;
-        this.movement = movement[this.type];
+        this.type = type;
     }
+
+    Piece.prototype.getColor = function () {
+        return this.color;
+    };
+
+    Piece.prototype.getMovement = function () {
+        return this.movement;
+    };
+
+    Piece.prototype.getPosition = function () {
+        return this.position;
+    };
+
+    Piece.prototype.getType = function () {
+        return this.type;
+    };
+
+    Piece.prototype.setColor = function (color) {
+        this.color = color;
+    };
+
+    Piece.prototype.setMovement = function (movement) {
+        this.movement = movement;
+    };
+
+    Piece.prototype.setPosition = function (position) {
+        this.position = position;
+    };
+
+    Piece.prototype.setType = function (type) {
+        this.type = type;
+    };
 
     return Piece;
 });
